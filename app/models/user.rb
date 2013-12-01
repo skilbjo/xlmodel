365a1b21fdcf2a8c332f68ca76d7b8071c6e0e71
John Skilbeck <john.skilbeck@gmail.com>
2013-12-01 15:30:23 -0800

    updated form

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ActiveRecord::Base
 	attr_accessor :name, :email, :ptoken
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i	
 
+	# have valid payment_processor & ptoken regex, change ptoken name to payment_token
+
 	validates :name, presence: true, length: { maximum: 50 }
 	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX } 
 									#	,uniqueness: { case_sensitive: false }
