9043ed0bc7545787e5229894b35562b3c053fd23
John Skilbeck <john.skilbeck@gmail.com>
2013-12-08 16:50:51 -0800

    updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Xlmodel::Application.routes.draw do
   resources :users
-  resources :fortumo
 
   root 'static_pages#home'
 
-  match '/12345/fortumo', to: 'users#index', 						via: 'get'
-  match '/help',     			to: 'static_pages#help',      via: 'get'
-  match '/about',   	 		to: 'static_pages#about',     via: 'get'
-  match '/contact',  			to: 'static_pages#contact',   via: 'get'
+  match '/2974087962/fortumo', 	to: 'users#index', 						via: 'get'
+  match '/help',     						to: 'static_pages#help',      via: 'get'
+  match '/about',   	 					to: 'static_pages#about',     via: 'get'
+  match '/contact',  						to: 'static_pages#contact',   via: 'get'
 
 end
